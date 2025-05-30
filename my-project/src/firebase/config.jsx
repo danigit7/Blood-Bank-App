cc0bd3687a1b0a30dece5ef19c6b8f13944e2adf
Daniyal <luffy999xx@gmail.com>
2025-05-30 23:09:27 +0500

    config.jsx

--- my-project/src/firebase/config.jsx
+++ my-project/src/firebase/config.jsx
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyBOefhJXmLQfNprfGs60ipV-sGQoVas3T8",
+  authDomain: "bloodbankapp-81863.firebaseapp.com",
+  projectId: "bloodbankapp-81863",
+  storageBucket: "bloodbankapp-81863.firebasestorage.app",
+  messagingSenderId: "971040097344",
+  appId: "1:971040097344:web:e10c375c7f20bc0d698ba5",
+  measurementId: "G-M9S7Y54W8D"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
